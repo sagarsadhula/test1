e2df8f694675e12fd9cd5e36427310dfc338e405
sagarsadhula <108025978+sagarsadhula@users.noreply.github.com>
2022-06-23 01:58:01 +0530

    commit my project

---
+++ SpringSecurity-1/src/main/java/com/example/demo/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping
+	public String sayWelcome() {
+		
+		return "Hi welcome to channel";
+	}
+
+}
